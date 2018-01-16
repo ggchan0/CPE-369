4b9a6229183248a80a73c983b024788d2b7f02f3
ggchan0 <ggchan@calpoly.edu>
2018-01-16 12:01:38 -0800

    lab2 cont

--- lab2/MapReducer.java
+++ lab2/MapReducer.java
@@ -1,17 +1,16 @@
 import java.io.*;
 import java.util.*;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.*;
+import org.apache.log4j.Logger;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
 
-public class MapReducer {
+public class MapReducer extends Configured implements Tool{
     public static class SalesMapper
             extends Mapper<LongWritable, Text, Text, IntWritable> {
         public void map(LongWritable key, Text value, Context context)
@@ -32,5 +31,37 @@ public class MapReducer {
 
             context.write(key, new IntWritable(count));
         }
+    }
+
+    private static final Logger THE_LOGGER =
+        Logger.getLogger(MapReducer.class);
+
+    public int run(String [] args) throws Exception {
+        Job job = Job.getInstance();
+        job.setJarByClass(MapReducer.class);
+        job.setJobName("MapReducer");
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        job.setMapperClass(SalesMapper.class);
+        job.setReducerClass(SalesReducer.class);
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        boolean status = job.waitForCompletion(true);
+        THE_LOGGER.info("run(): status=" + status);
+        return status ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("usage: <input> <output>");
+        }
+
+        THE_LOGGER.info("inputDir = " + args[0]);
+        THE_LOGGER.info("outputDir = " + args[1]);
+        int returnStatus = ToolRunner.run(new MapReducer(), args);
+        THE_LOGGER.info("returnStatus=" + returnStatus);
+        System.exit(returnStatus);
     }
 }
